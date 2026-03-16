903cbea8582afb15d77d06718ed9e8302a1b7e72
dacop1 <dcop3009@gmail.com>
2026-03-16 22:45:09 +0100

    Delete Unidad_1 /Act 007. NumeroParImpar.py

--- Unidad_1 /Act 007. NumeroParImpar.py
+++
@@ -1,9 +0,0 @@
-#solicitamos un número al usuarios
-numero = int(input("Ingresa un numero para saber si es par o impar:"))
-#usamos un condicional
-if numero % 2 == 0:
-    print("El numero", numero, "es par")
-else:
-    print("el numero", numero, "es impar")
-
-
